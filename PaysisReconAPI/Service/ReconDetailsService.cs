a0c5895c0a6e6b5906800b3baa0e3db65f39ce99
mandarpalse <mandar.palse@india1.co.in>
2026-03-24 21:33:01 +0530

    Commit all

--- PaysisReconAPI/Service/ReconDetailsService.cs
+++ PaysisReconAPI/Service/ReconDetailsService.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using PaysisReconAPI.DatabaseContext;
 using PaysisReconAPI.Model;
 using PaysisReconAPI.Repository;
@@ -44,3 +45,51 @@ namespace PaysisReconAPI.Service
 
     }
 }
+=======
+﻿using PaysisReconAPI.DatabaseContext;
+using PaysisReconAPI.Model;
+using PaysisReconAPI.Repository;
+
+namespace PaysisReconAPI.Service
+{
+    public class ReconDetailsService
+    {
+        ReconDetailsRepository ObjCommonRepository = null;
+        public ReconDetailsService(IDataDbContext db)
+        {
+            ObjCommonRepository = new ReconDetailsRepository(db);
+        }
+
+        public bool GetReconConfirmedDetailsServices(string businessdate, string network)
+        {
+            return ObjCommonRepository.GetReconConfirmedDetails(businessdate, network);
+        }
+
+        public string ReconExistsConfirmationServices(string businessdate, string network)
+        {
+            return ObjCommonRepository.GetReconExistsConfirmation(businessdate, network);
+        }
+
+        public string PutReconConfirmationDetailsServices(string businessdate, string userid, string network)
+        {
+            return ObjCommonRepository.PutReconConfirmationDetails(businessdate, userid, network);
+        }
+
+        public List<RecongroupList> GetReconGroupListServices(string businessdate, string network)
+        {
+            return ObjCommonRepository.GetReconGroupList(businessdate, network);
+        }
+
+        public List<ReconStatusMaster> GetReconStatusMasterDetailsServices(string businessdate, string network)
+        {
+            return ObjCommonRepository.GetReconStatusMasterDetails(businessdate, network);
+        }
+
+        public string Raise_request_reconServices(int recongroupid, string businessdate, int requestby)
+        {
+            return ObjCommonRepository.Raise_request_recon(recongroupid, businessdate, requestby);
+        }
+
+    }
+}
+>>>>>>> dcdba96e7f094d497b4cc8da0179d85a2c509b31
